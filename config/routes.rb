cf53638af0e32a5d925f4743f5e7fe6021a7feaf
Chiara Campalani <campalanichiara@gmail.com>
2024-03-27 18:57:54 +0900

    created routes for Items & Item_Amounts

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :items do
+    resources :item_amounts, only: %i[new create edit update]
+
+    # collection do
+    #   get :expiring_soon
+    #   get :expired
+    # end
+  end
+  resources :item_amounts, only: [:destroy]
+  root to: 'items#home'
 end
