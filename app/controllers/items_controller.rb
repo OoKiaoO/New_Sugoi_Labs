c3faad916f2af14bbae80b4c5f93a5b478e1cf90
Chiara Campalani <campalanichiara@gmail.com>
2024-04-05 12:42:12 +0900

    created Home page view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,115 @@
 class ItemsController < ApplicationController
+
+  def home
+    # items = Item.all
+    # current_month = Date.today
+    # next_month = Date.today.next_month
+    # next_next_month = Date.today.next_month(2)
+    
+    # @current_month_items = get_monthly_items(items, get_monthly_range(current_month))
+    # @next_month_items = get_monthly_items(items, get_monthly_range(next_month))
+    # @next_next_month_items = get_monthly_items(items, get_monthly_range(next_next_month))
+  end
+
+  def index
+    # TODO: make filters apply to prev. searched query results only, if present
+    # add conditional in each filter 
+    # @items = params[:filter].present? ? Item.search_by_all_item_info(params[:query]).order(brand: :asc) : ...?
+    # if params[:query].present?
+    #   @items = Item.search_by_all_item_info(params[:query])
+    # elsif params[:filter].present? && params[:filter] == 'brand'
+    #   @items = Item.all.order(brand: :asc)
+    # elsif params[:filter].present? && params[:filter] == 'category'
+    #   @items = Item.all.order(category: :asc)
+    # elsif params[:filter].present? && params[:filter] == 'location'
+    #   @items = Item.all.order(location: :asc)
+    # else
+    #   @items = Item.all.order(name: :asc)
+    # end
+  end
+
+  # def show
+  #   @item_amount = ItemAmount.new
+  #   @unchecked_amounts = @item.item_amounts.select {|amount| !amount.checked}
+    
+  #   unless @item.item_amounts.empty?
+  #     if @item.item_amounts.count == 1
+  #       @item_amounts = @item.item_amounts.order(created_at: :desc)
+  #       @data_values = [@item.item_amounts.first.amount]
+  #       @data_keys = ['Expiring next']
+  #     else
+  #       chart_data = get_chart_data
+  #       @data_values = chart_data[:data_values]
+  #       @data_keys = chart_data[:data_keys]
+  #       @exp_amounts = @item.item_amounts.select {|amount| amount.checked}
+  #       @waste_data = get_waste_chart_data(@exp_amounts)
+  #       @data_waste_keys = @waste_data.map {|item| item[:month] }
+  #       @data_waste_values = @waste_data.map {|item| item[:total] }
+
+  #       if params[:option] == 'amount#reload'
+  #         @item_amounts = @item.item_amounts.order(amount: :desc)
+  #       elsif params[:option] == 'exp#reload'
+  #         @item_amounts = @item.item_amounts.order(exp_date: :asc)
+  #       elsif params[:option] == 'remaining'
+  #         @item_amounts = @item.item_amounts.order(exp_date: :asc)
+  #       else
+  #         @item_amounts = @item.item_amounts.order(created_at: :desc)
+  #       end
+  #     end
+  #   end
+  # end
+
+  # def new
+  #   @item = Item.new
+  # end
+
+  # def create
+  #   @item = Item.new(item_params)
+  #   @item.save!
+
+  #   if @item.save
+  #     log(@item.id, "Created new item")
+  #   end
+
+  #   redirect_to item_path(@item)
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   @item.update(item_params)
+
+  #   if @item.save
+  #     log(@item.id, "Updated item's info")
+  #   end
+
+  #   redirect_to item_path(@item)
+  # end
+
+  # def destroy
+  #   @item.destroy
+
+  #   redirect_to items_path
+  # end
+
+
+  # private
+
+
+  # def item_params
+  #   params.require(:item).permit(:name,
+  #                                :brand, 
+  #                                :barcode, 
+  #                                :secondary_barcode, 
+  #                                :description, 
+  #                                :category, 
+  #                                :location, 
+  #                                :retail, 
+  #                                :photo)
+  # end
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 end
