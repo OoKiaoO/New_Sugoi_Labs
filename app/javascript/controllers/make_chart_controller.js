e15ea4166a827362347ddb3d7abdba571b2723fe
Chiara Campalani <campalanichiara@gmail.com>
2024-05-06 18:35:19 +0900

    fixed chart.js package import bug

--- app/javascript/controllers/make_chart_controller.js
+++ app/javascript/controllers/make_chart_controller.js
@@ -1,5 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
-import Chart from "chart.js"
+// if page gives error or chart doesn't load, make sure to run 
+// bin/rails assets:precompile before reloading to avoid cached content
+
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 // Connects to data-controller="make-chart"
 export default class extends Controller {
@@ -8,14 +12,17 @@ export default class extends Controller {
     labels: Array,
     data: Array
   }
+
+  canvasContext() {
+    return this.stockChartTarget.getContext('2d');
+  }
   
   connect() {
-    console.log(this.labelsValue)
-    console.log(this.dataValue)
-
+    // console.log(this.stockChartTarget)
+    // console.log(this.labelsValue)
+    // console.log(this.dataValue)
     
-    new Chart(
-      this.stockChartTarget,
+    new Chart(this.canvasContext(),
       {
         type: 'pie',
         data: {
@@ -32,27 +39,6 @@ export default class extends Controller {
           ]
         }
       }
-    );
+    )
   }
-
-  
-  // if () {
-  //   // const ctx = myChart.getContext('2d');
-  //   if (ctx) {
-  //     const myChart = new Chart(ctx, {
-  //       type: 'pie',
-  //       data: {
-  //         labels: JSON.parse(ctx.canvas.dataset.labels),
-  //         datasets: [{
-  //           data: JSON.parse(ctx.canvas.dataset.data),
-  //           backgroundColor: [
-  //             'rgba(234, 60, 182, 0.8)',
-  //             'rgba(246, 135, 45, 0.8)',
-  //             'rgba(95, 60, 234, 0.8)'
-  //           ]
-  //         }]
-  //       },
-  //       });
-  //   }
-  // }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "chart.js" # @4.4.2
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@latest/dist/chart.min.js"
+# pin "chart.js" # @4.4.2
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
